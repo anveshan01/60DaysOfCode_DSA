c21403839fa396f1ef29e524f6829560dcacc086
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2025-03-14 10:00:36 +0530

    Day-2 of 60DaysOfCode_DSA

--- Vanshika_2027/13-03-2025.java
+++ Vanshika_2027/13-03-2025.java
@@ -52,3 +52,47 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+class Solution {
+    public int findMin(int[] nums) {
+        int s =0, e = nums.length-1;
+        int min = Integer.MAX_VALUE;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[s]<=nums[e]){
+                min = Math.min(min,nums[s]);
+                break;
+            }
+            if(nums[s]<=nums[m]){
+                min = Math.min(min,nums[s]);
+                s=m+1;
+            }else{
+                min = Math.min(min,nums[m]);
+                e=m-1;
+            }
+        }
+        return min;
+    }
+}
+
+//https://leetcode.com/problems/merge-intervals/submissions/1573145202/
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return a[0]-b[0];
+            }
+        });
+        List<int[]> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]) {
+                ans.add(intervals[i]);
+            } else {
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
