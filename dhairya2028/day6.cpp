c363ece347193f3b96999fd0c9229d95ebde50b2
Dhairyagarg812 <dhairyagarg812@gmail.com>
2025-03-17 20:17:01 +0530

    Stock Buy & Sell leetcode

---
+++ dhairya2028/day6.cpp
@@ -0,0 +1,14 @@
+//problem Stock Buy & Sell
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int bestbuy = prices[0], maxprofit = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < bestbuy)
+                bestbuy = prices[i];
+            maxprofit = max(maxprofit, (prices[i] - bestbuy));
+        }
+        return maxprofit;
+    }
+};
