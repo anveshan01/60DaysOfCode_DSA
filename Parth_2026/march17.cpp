53534726fe5b819b82f04f10353762f5282e75ea
parth-n <parth.nepalia@gmail.com>
2025-03-17 06:59:09 +0530

    march17

---
+++ Parth_2026/march17.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        bool divideArray(vector<int>& nums) {
+            unordered_map<int,int> mp ;
+            for(auto num : nums){
+                mp[num]++ ;
+            }
+            for(auto it : mp){
+                if(it.second%2!=0)    return false ;
+            }
+            return true ;
+        }
+    };
