a51a8d1a70eccdb07e795c3baf984ece7b3d533d
Dhairya Summi <dhairya.summi@gmail.com>
2025-03-17 19:43:26 +0000

    done

---
+++ Dhairya_2027/Day6.cpp
@@ -0,0 +1,15 @@
+// 17-03-2025
+// POTD LEETCODE 17
+
+class Solution {
+    public:
+        bool divideArray(vector<int>& nums) {
+            sort(nums.begin() , nums.end());
+    
+            for(int i = 0 ; i < nums.size()-1 ; i+=2){
+                if(nums[i] != nums[i+1])
+                    return false;
+            }
+            return true;
+        }
+    };
