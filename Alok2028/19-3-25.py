c9d4154112769da20cc93c69fca5b68eb6e6d358
alok-psr <168848919+alok-psr@users.noreply.github.com>
2025-03-19 23:16:31 +0530

    problem 237: deleting node without knowing the head of the linked list

---
+++ Alok2028/19-3-25.py
@@ -0,0 +1,18 @@
+# 237. Delete Node in a Linked List
+# date: 19-3-25
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node and node.next:
+            node.val=node.next.val
+            node.next=node.next.next
