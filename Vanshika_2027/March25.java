ab094861f9575b101e0e4363c2059a99a92363d9
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2025-03-26 09:44:26 +0530

    Day-14 of 60DaysOfCode_DSA

---
+++ Vanshika_2027/March25.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root.val == p.val || root.val == q.val){
+            return root;
+        }
+        TreeNode leftLCA = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right,p,q);
+        if(leftLCA == null) return rightLCA;
+        if(rightLCA == null) return leftLCA;
+        return root;
+    }
+}
+//
