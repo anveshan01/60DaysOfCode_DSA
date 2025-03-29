4895c1441ab2bd089fcebee30179a1847260a45e
Dhairyagarg812 <dhairyagarg812@gmail.com>
2025-03-30 00:40:28 +0530

    Create day18.cpp

---
+++ dhairya2028/day18.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int st=0,end=x;
+        while(st<=end){
+            long long mid=st+(end-st)/2;
+            if(mid*mid>x) end=mid-1;
+            else if(mid*mid<x) st=mid+1;
+            else return mid;
+        }
+        return end;
+        
+    }
+    
+};
