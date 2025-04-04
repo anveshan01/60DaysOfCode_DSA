da7909c78ca2c7cb29d9e68d117ec893cbe013c1
Daksh0027 <dakshshastri72@gmail.com>
2025-04-04 23:09:17 +0530

    24

---
+++ Daksh_28/04-04-2025.cpp
@@ -0,0 +1,35 @@
+// LeetCode 1652
+class Solution
+{
+public:
+    vector<int> decrypt(vector<int> &code, int k)
+    {
+        vector<int> res(code.size(), 0);
+        int n = code.size();
+        if (k == 0)
+            return res;
+        for (int i = 0; i < n; ++i)
+        {
+            int sum = 0;
+            if (k > 0)
+            {
+                for (int j = 1; j <= k; ++j)
+                {
+                    sum += code[(i + j) % n];
+                }
+            }
+            else if (k < 0)
+            {
+                for (int j = 1; j <= -k; ++j)
+                {
+                    sum += code[(i - j + n) % n];
+                }
+            }
+            res[i] = sum;
+        }
+        return res;
+    }
+};
+
+// T.C. O(n^2)
+// S.C. O(n)
