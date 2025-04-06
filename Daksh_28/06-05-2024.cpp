8ff1509fdc240776a52284b4a82ef9a150636e43
Daksh0027 <dakshshastri72@gmail.com>
2025-04-06 22:11:07 +0530

    26

---
+++ Daksh_28/06-05-2024.cpp
@@ -0,0 +1,45 @@
+// LeetCode 368
+class Solution
+{
+public:
+    vector<int> largestDivisibleSubset(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+            return {};
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> dp(n, 1);
+        vector<int> prev(n, -1);
+        int maxIdx = 0;
+
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i])
+                {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > dp[maxIdx])
+            {
+                maxIdx = i;
+            }
+        }
+        vector<int> res;
+        while (maxIdx != -1)
+        {
+            res.push_back(nums[maxIdx]);
+            maxIdx = prev[maxIdx];
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+// T.C. O(n^2)
+// S.C. O(n)
