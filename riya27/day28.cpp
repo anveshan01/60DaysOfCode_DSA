07e23013c5009cf905f46f6fd2869d0c12a8c3de
verma-riya20 <riyaverma3731@gmail.com>
2025-04-08 20:02:41 +0530

    day28

---
+++ riya27/day28.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int minimumOperations(vector<int>& nums) {
+            int n=nums.size();
+            vector<bool>vis(128);
+            for(int i=n-1;i>=0;i--){
+               if(vis[nums[i]]){
+                   return i/3 +1;
+               }
+               vis[nums[i]]=true;
+            }
+           return 0; 
+        }
+    };
