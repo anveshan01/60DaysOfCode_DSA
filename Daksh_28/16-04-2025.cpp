9f6fea9974f65285fb7c636114016411e8b6ab45
Daksh0027 <dakshshastri72@gmail.com>
2025-04-16 16:23:38 +0530

    36

---
+++ Daksh_28/16-04-2025.cpp
@@ -0,0 +1,20 @@
+// LeetCode 258
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        else if (num % 9 == 0)
+        {
+            return 9;
+        }
+        else
+            return num % 9;
+    }
+};
+// T.C = O(1)
+// S.C = O(1)
