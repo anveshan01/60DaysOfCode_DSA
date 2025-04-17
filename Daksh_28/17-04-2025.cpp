ad5dd7cfc3a9ce4032092deb5a451b4eaf238b14
Daksh0027 <dakshshastri72@gmail.com>
2025-04-17 22:26:30 +0530

    37

---
+++ Daksh_28/17-04-2025.cpp
@@ -0,0 +1,23 @@
+// 2176 LeetCode
+class Solution
+{
+public:
+    int countPairs(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if ((i * j) % k == 0 && nums[i] == nums[j])
+                {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
+// T.C O(n^2)
+// S.C O(1)
