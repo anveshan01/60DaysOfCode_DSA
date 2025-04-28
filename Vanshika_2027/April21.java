8e079f477b1d6c99696b8b5e9a4c7c8d1943bb80
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2025-04-28 22:30:05 +0530

    Day-41 of 60DaysOfCode_DSA

---
+++ Vanshika_2027/April21.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/coin-change-ii/submissions/
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int c : coins) {
+            for (int i = c; i <= amount; i++) {
+                dp[i] += dp[i-c];
+            }
+        }
+        return dp[amount];
+    }
+}
