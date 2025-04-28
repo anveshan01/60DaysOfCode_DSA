e456dd9c67397459f20b90da8dff14074f552dd8
parth-n <parth.nepalia@gmail.com>
2025-04-28 07:55:42 +0530

    apr21

---
+++ Parth_2026/appr21.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long min=0 ,max=0 , sum = 0 ;
+        for(int ele : differences){
+            sum+=ele ;
+            min = Math.min(sum,min) ;
+            max = Math.max(sum,max) ;
+        }
+
+        return (int)Math.max(0L, (upper-max)-(lower-min)+1L) ;
+    }
+}
