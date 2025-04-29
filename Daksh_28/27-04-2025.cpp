a2111827b9cf0a9793d553b3be5e96b5bd2573b4
Daksh <dakshshastri72@gmail.com>
2025-04-29 18:30:40 +0530

    47

---
+++ Daksh_28/27-04-2025.cpp
@@ -0,0 +1,15 @@
+// 3392 LeetCode 
+class Solution {
+    public:
+        int countSubarrays(vector<int>& nums) {
+            int count = 0;
+            for(int i = 0; i + 2 < nums.size(); i++){
+                if (2 * (nums[i] + nums[i+2]) == nums[i+1]){
+                    count++;
+                }
+            }
+            return count;
+        }
+    };
+//T.C = O(n)
+//S.C = O(1)
