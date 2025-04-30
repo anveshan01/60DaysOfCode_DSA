198e5204539c0b1e35b42e6f809c34b879ba3271
Daksh <dakshshastri72@gmail.com>
2025-04-30 22:56:55 +0530

    49

---
+++ Daksh_28/30-04-2025.cpp
@@ -0,0 +1,21 @@
+// 1295 LeetCode 
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {
+            int c=0;
+            for (int i=0;i<nums.size();i++){
+                int n=0;
+                int x=nums[i];
+                while(x!=0){
+                    n++;
+                    x=x/10;
+                }
+                if (n%2==0){
+                    c++;
+                }
+            }
+            return c;
+        }
+    };
+// T.C: O(n)
+// S.C: O(1)
