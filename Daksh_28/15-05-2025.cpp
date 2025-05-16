9c881304a0d6de70a5703662eacb3ea6c6c66077
Daksh <dakshshastri72@gmail.com>
2025-05-16 21:54:16 +0530

    64

---
+++ Daksh_28/15-05-2025.cpp
@@ -0,0 +1,16 @@
+// LeetCode 
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> ans;
+        int n = words.size();
+        for(int i = 0; i < n; i++){
+            if(i == 0 || groups[i] != groups[i-1]){
+                ans.emplace_back(words[i]);
+            }
+        }
+        return ans;
+    }
+};
+// T.C : O(n)
+// S.C : O(n)
