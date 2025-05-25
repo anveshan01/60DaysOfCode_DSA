7be9547d6cb0d00239a07a6f16303ff7882c9522
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2025-05-25 15:18:01 +0530

    Day-62 of 100DaysOfCode_DSA

---
+++ Vanshika_2027/May12.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/three-consecutive-odds
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count=0;
+        for(int num:arr) {
+            if(num%2 != 0) {
+                count++;
+                if(count==3) {
+                    return true;
+                }
+            }else{
+                count=0;
+            }
+        }
+        return false;
+    }
+}
