36a8edccbbec299658497d367b1aa207b4eaf076
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2025-05-25 15:08:31 +0530

    Day-45 of 60DaysOfCode_DSA

---
+++ Vanshika_2027/April25.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/jump-game
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxI = 0;
+        for(int i =0; i<nums.length; i++){
+            if(i>maxI) return false;
+            maxI = Math.max(maxI, i+nums[i]);
+        }
+        return true;
+    }
+}
