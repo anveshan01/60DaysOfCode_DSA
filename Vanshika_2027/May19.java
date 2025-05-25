1d74fb167c1951beed2c951d7c2384dc7d8087e4
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2025-05-25 15:23:18 +0530

    Day-69 of 100DaysOfCode_DSA

---
+++ Vanshika_2027/May19.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/partition-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode leftH = new ListNode(0);
+        ListNode rightH = new ListNode(0);
+
+        ListNode left = leftH;
+        ListNode right = rightH;
+
+        while (head != null) {
+            if (head.val < x) {
+                left.next = head;
+                left = left.next;
+            } else {
+                right.next = head;
+                right = right.next;
+            }
+            head = head.next;
+        }
+
+        right.next = null;
+        left.next = rightH.next;
+        return leftH.next;
+    }
+}
