3ea3b0979f9ec0e547c823ef2b0f4417f1489370
Daksh <dakshshastri72@gmail.com>
2025-06-01 10:00:42 +0530

    81

---
+++ Daksh_28/01-06-2025.cpp
@@ -0,0 +1,16 @@
+// LeetCode 2929
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long ans = 0;
+        for (int i = 0; i <= min(limit, n); i++) {
+            if (n - i > 2 * limit) {
+                continue;
+            }
+            ans += min(n - i, limit) - max(0, n - i - limit) + 1;
+        }
+        return ans;
+    }
+};
+// T.C: O(n)
+// S.C: O(1)
