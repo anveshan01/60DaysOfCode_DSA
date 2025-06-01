215479965ac7ec18f619ace2093f29e5d32e5ad8
Daksh <dakshshastri72@gmail.com>
2025-06-01 09:58:27 +0530

    79

---
+++ Daksh_28/30-05-2025.cpp
@@ -0,0 +1,12 @@
+// LeetCode 561 
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int n = nums.size(), sum = 0;
+        sort(nums.begin() , nums.end());
+        for(int i = 0; i < n; i += 2) sum += nums[i];
+        return sum;
+    }
+};
+// T.C: O(nlogn)
+// S.C: O(1)
